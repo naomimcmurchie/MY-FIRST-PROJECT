b17ddbb0062ca1382f32722aed35455e4ee91dd2
naomimcmurchie <chenguomi@outlook.com>
2020-01-01 22:16:25 +0000

    second job

---
+++ get_user.py
@@ -0,0 +1,13 @@
+# this is my second program
+# This program will get input on the users.
+
+
+
+name = raw_input('please input your name ')
+full_name = str(name)
+
+
+
+
+
+print ('Hello ' + full_name)
