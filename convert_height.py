4f812423ebb54896c759cea5e5ea4506ed018c5c
naomimcmurchie <chenguomi@outlook.com>
2020-01-01 23:01:56 +0000

    height convert

---
+++ convert_height.py
@@ -0,0 +1,20 @@
+# this is my second program
+# This program will get input on the users.
+# This program will also convert their height
+
+# get the user name
+name = raw_input('please input your name ')
+full_name = str(name)
+print ('Hello ' + full_name)
+
+# get the user height
+height = raw_input('please input your height in cm: ')
+height = int(height) * 0.01
+print('your height in m is')
+print(str(height) + 'm')
+
+
+# convert the user height
+height_in_feet = height * 3.281
+height_in_feet = round(height_in_feet, 2 )
+print ('your height in feet is ' + str(height_in_feet) + ' feet ')
